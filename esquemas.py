0fed16bd7fd8eb1f7f87846966ca8848a00c0dcb
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-17 14:33:36 -0500

    new schema

--- esquemas.py
+++ esquemas.py
@@ -19,3 +19,8 @@ class Movie(BaseModel):
                 "category" : "Drama"
             }
         }
+
+class User(BaseModel):
+    email : str = Field(..., min_length=1, max_length=50)
+    password : str = Field(..., min_length=1, max_length=50)
+    
