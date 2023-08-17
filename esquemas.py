276f7dec80f05577b2cad691ed15a0300c52e850
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-08-16 21:48:27 -0500

    validation schema

--- esquemas.py
+++ esquemas.py
@@ -4,7 +4,18 @@ from typing import Optional
 class Movie(BaseModel):
     id : Optional[int] = None
     title : str = Field(..., min_length=1, max_length=50)
-    overview : str | None = None
-    year : str
-    rating : float | None = None
-    category : str
+    overview : str = Field(..., min_length=1, max_length=50)
+    year : str = Field(...,le=2021)
+    rating : float = Field(...,ge=0.0,le=10.0)
+    category : str = Field(..., min_length=1, max_length=20)
+
+    class Config :
+        json_schema_extra ={
+            "example" : {
+                "title" : "The Godfather",
+                "overview" : "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+                "year" : "1972",
+                "rating" : 9.2,
+                "category" : "Drama"
+            }
+        }
